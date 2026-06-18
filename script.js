ebbcce88184d1248d68f53ceba9ef24e82e23308
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-06-18 13:02:47 +0700

    3D Maker With a .html n .css n .js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (kartuAli) {
         VanillaTilt.init(kartuAli, {
-            max: 20,           // Batas kemiringan maksimal (derajat)
-            speed: 400,        // Kecepatan respon balik kartu
-            glare: true,       // Mengaktifkan kilauan cahaya pas miring
-            "max-glare": 0.3,  // Terang kilauan cahaya
-            scale: 1.05        // Kartu membesar sedikit saat di-hover mouse
+            max: 20,           
+            speed: 400,        
+            glare: true,       
+            "max-glare": 0.3,  
+            scale: 1.05,       
+            
+            gyroscope: true,
+            gyroscopeMinAngleX: -45,
+            gyroscopeMaxAngleX: 45, 
+            gyroscopeMinAngleY: -45,
+            gyroscopeMaxAngleY: 45  
         });
     }
 });
